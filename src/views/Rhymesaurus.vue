dca822964898a3d6213da5383242d55fee68bacb
dfoong <foongd@seattleu.edu>
2019-03-09 12:04:43 -0800

    just added vue to the ghibli app, foun dation

--- src/views/Rhymesaurus.vue
+++ src/views/Rhymesaurus.vue
@@ -1,58 +1,36 @@
 <template>
   <div class="rhymesaurus">
-    <form><!-- TODO: Use a submit event handler to allow the findWords method to handle this form submission. -->
-      <p>Find rhymes for <input type="text" v-model="rhyme"> related to <input type="text" v-model="phrase"> <button type="submit">Search</button></p>
-    </form>
-    <!-- TODO: Add a v-if conditional to make this results list show only if there are results and if the length is greater than 0. -->
-    <ul class="results">
-      <!-- TODO: Add a v-for loop to the LI tag to loop through the items in the results. -->
-      <li class="item">
-        <p><strong><!-- TODO: Output word --></strong></p>
-        <p><!-- TODO: Output score --></p>
-      </li>
-    </ul>
-
-    <!-- TODO: Add a `v-else-if` conditional to make this message only show if there are no results returned (but we have actually attempted a request). -->
-    <div class="no-results">
-      <h2>No Words Found</h2>
-      <p>Please adjust your search to find more words.</p>
+    <div class="container">
+      <div v-for="result in results" class="card" :key="result.id">
+        <h1>{{result.title}}</h1>
+        <p>{{result.description}}</p>
+      </div>
     </div>
-
-    <!-- TODO: Add a v-if conditional to make this errors list show only if there are errors and if the length is greater than 0. -->
-    <ul class="errors">
-      <!-- TODO: Add a v-for loop to the LI tag to loop through the errors. -->
-      <li>
-        <!-- TODO: Output each error. -->
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-// TODO: Import axios properly here.
+import axios from "axios";
 
 export default {
-  name: 'Rhymesaurus',
-  data () {
+  name: "Rhymesaurus",
+  data() {
     return {
-      results: null,
-      errors: [],
-      phrase: '',
-      rhyme: ''
-    }
+      results: [],
+      errors: []
+    };
+  },
+  created: function() {
+    axios
+      .get("https://ghibliapi.herokuapp.com/films", {})
+      .then(response => {
+        this.results = response.data;
+      })
+      .catch(error => {
+        this.errors.push(error);
+      });
   }
-    // TODO: Create the findWords method.
-
-    // TODO: Complete the following inside of the findWords method.
-      // TODO: Create an axios.get statement that requests from https://api.datamuse.com/words
-      // TODO: Create the params object
-      // TODO: Set the `ml` parameter equal to `this.phrase`
-      // TODO: Set the `rel_ehy` parameter equal to `this.rhyme`
-      // TODO: Create a `then` clause
-      // TODO: Inside the `then` clause, set `this.results` equal to `response.data`
-      // TODO: Create a `catch` clause
-      // TODO: Inside the `catch` clause, push the new `error` onto the `this.errors` array
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -61,7 +39,7 @@ export default {
   font-size: 1.4rem;
 }
 
-input[type="text"]{
+input[type="text"] {
   border-top: none;
   border-left: none;
   border-right: none;
@@ -70,10 +48,10 @@ input[type="text"]{
   font-size: 1.4rem;
   color: #2c3e50;
   font-weight: 300;
-  background: rgba(0,0,0,0.02);
+  background: rgba(0, 0, 0, 0.02);
   padding: 0.5rem;
 }
-button{
+button {
   background: #333;
   padding: 0.5rem;
   font-weight: 300;
@@ -82,7 +60,8 @@ button{
   cursor: pointer;
   font-size: 1.4rem;
 }
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -99,7 +78,7 @@ ul.results {
   width: 200px;
   min-height: 100px;
   color: #fff;
-  background: rgba(0,0,0,0.7);
+  background: rgba(0, 0, 0, 0.7);
 }
 ul.errors {
   list-style-type: none;
